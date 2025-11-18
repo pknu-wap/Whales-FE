0057fc99c7784f969b0e2ff4ce4783c530ae5f58
Minjun Jeong <minjunjeong055@gmail.com>
2025-11-18 23:11:06 +0900

    server
    수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '@/stores/authStore';
 
 // API 기본 URL 설정
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://3.27.82.185:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://3.27.115.110:8080/api';
 
 // axios 인스턴스 생성
 const api = axios.create({
