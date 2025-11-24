a641879d9173edafd8ca5990d3174d393ff854be
Minjun Jeong <minjunjeong055@gmail.com>
2025-11-24 17:41:10 +0900

    fix-임포트
    사용되지 않는 임포트 제거

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -49,9 +49,9 @@ export default function PostDetail() {
   const [isDisliked, setIsDisliked] = useState(false);
   const [isScraped, setIsScraped] = useState(false);
 
-  const handleTagClick = (tag: string) => {
-    navigate(`/search?tag=${encodeURIComponent(tag)}`)
-  }
+  // const handleTagClick = (tag: string) => {
+  //   navigate(`/search?tag=${encodeURIComponent(tag)}`)
+  // }
 
   useEffect(() => {
     if (!id) {
