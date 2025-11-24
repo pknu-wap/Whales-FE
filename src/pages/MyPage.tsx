b3ff525e5ea3171369192ceca2791974a6204cb2
Minjun Jeong <minjunjeong055@gmail.com>
2025-11-24 17:25:40 +0900

    fix-사이드바
    마이페이지에서 사이드바 길이를 고침

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -341,8 +341,8 @@ export default function MyPage() {
   };
 
   return (
-    <div className="min-h-screen bg-background">
-      <main className="w-full max-w-7xl mx-auto flex p-6 gap-6">
+    <div>
+      <main className="w-full max-w-7xl mx-auto flex p-6 gap-6 items-start">
         <AppSidebar />
 
         <section className="flex-1 flex flex-col gap-6">
